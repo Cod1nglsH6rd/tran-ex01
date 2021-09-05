ddcb794c7182c037c21083df213b63733be4015e
Cod1nglsH6rd <gialam.d.tran@gmail.com>
2021-09-05 19:26:05 -0400

    The first excercise of cop3330

--- src/main/java/Solution01.java
+++ src/main/java/Solution01.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Solution01 {
     public static void main(String[] args){
         /*
-        Ask for name
-            Reads input = "name"
-        Output message with name
+        Print "What is your name?"
+            Input = "name"
+        Print "Hello *name*, nice to meet you!"
          */
         Scanner input = new Scanner(System.in);
         System.out.print("What is your name?");
         String name = input.nextLine();
-        System.out.printf("Hello, %s, nice to meet you!", name);
+        System.out.printf("Hello %s, nice to meet you!", name);
     }
 
 }
